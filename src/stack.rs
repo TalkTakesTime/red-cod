dfab5fe4c59cee046cc7ebe01502ef2730507464
TalkTakesTime <b3nf@hotmail.com>
2019-01-02 18:41:09 +1300

    Implement most stack manipulation operators

--- src/stack.rs
+++ src/stack.rs
@@ -1,15 +1,12 @@
 use std::collections::VecDeque;
-
-pub struct Stack {
-    entries: VecDeque<char>,
-    register: Option<char>,
-}
+use std::iter::FromIterator;
 
 pub struct ProgramStack {
     base: Stack,
     children: Vec<Stack>,
 }
 
+#[derive(Debug)]
 pub enum StackError {
     Underflow,
 }
@@ -20,19 +17,188 @@ impl ProgramStack {
     }
 
     pub fn new_stack(&mut self, n: usize) -> Result<(), StackError> {
-        let s_len = self.curr().len();
-        if n > s_len {
-            Err(StackError::Underflow)
-        } else {
-            let new_stack = self.curr().split_off(s_len - n);
-            self.children.push(new_stack);
-            Ok(())
-        }
+        let new_stack = self.curr().split(n)?;
+        self.children.push(new_stack);
+        Ok(())
     }
 
     pub fn drop_stack(&mut self) {
-        if self.children.len() > 0 {
-
+        if let Some(top) = self.children.pop() {
+            self.curr().extend(top);
+        } else {
+            self.curr().clear();
         }
+    }
+}
+
+pub struct Stack {
+    entries: VecDeque<f64>,
+    register: Option<f64>,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            entries: VecDeque::new(),
+            register: None,
+        }
+    }
+
+    pub fn pop(&mut self) -> Result<f64, StackError> {
+        self.entries.pop_back().ok_or(StackError::Underflow)
+    }
+
+    pub fn push(&mut self, val: f64) {
+        self.entries.push_back(val);
+    }
+
+    pub fn clear(&mut self) {
+        self.register = None;
+        self.entries.clear();
+    }
+
+    pub fn split(&mut self, n: usize) -> Result<Self, StackError> {
+        let self_len = self.entries.len();
+        if self_len < n {
+            Err(StackError::Underflow)
+        } else {
+            let s = self.entries.split_off(self_len - n);
+            Ok(s.into_iter().collect())
+        }
+    }
+
+    // +
+    pub fn add(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(y + x);
+        Ok(())
+    }
+
+    // -
+    pub fn subtract(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(y - x);
+        Ok(())
+    }
+
+    // *
+    pub fn multiply(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(y * x);
+        Ok(())
+    }
+
+    // ,
+    pub fn divide(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(y / x);
+        Ok(())
+    }
+
+    // %
+    pub fn modulo(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(y % x);
+        Ok(())
+    }
+
+    // =
+    pub fn equals(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(if y == x { 1f64 } else { 0f64 });
+        Ok(())
+    }
+
+    // )
+    pub fn greater_than(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(if y > x { 1f64 } else { 0f64 });
+        Ok(())
+    }
+
+    // (
+    pub fn less_than(&mut self) -> Result<(), StackError> {
+        let x = self.pop()?;
+        let y = self.pop()?;
+        self.push(if y < x { 1f64 } else { 0f64 });
+        Ok(())
+    }
+
+    // :
+    pub fn dup(&mut self) -> Result<(), StackError> {
+        let val = self.entries.back().ok_or(StackError::Underflow)?;
+        self.push(*val);
+        Ok(())
+    }
+
+    // $ and @
+    pub fn swap(&mut self, n: usize) -> Result<(), StackError> {
+        let len = self.entries.len();
+        if n > len {
+            return Err(StackError::Underflow);
+        }
+
+        for i in (len - 1)..(len - n) {
+            self.entries.swap(i, i - 1);
+        }
+        Ok(())
+    }
+
+    // }
+    pub fn shift_right(&mut self) {
+        if let Some(val) = self.entries.pop_back() {
+            self.entries.push_front(val);
+        }
+    }
+
+    // {
+    pub fn shift_left(&mut self) {
+        if let Some(val) = self.entries.pop_front() {
+            self.entries.push_back(val);
+        }
+    }
+
+    // r
+    pub fn reverse(&mut self) {
+        let vals: Vec<_> = self.entries.drain(..).collect();
+        for val in vals {
+            self.entries.push_front(val);
+        }
+    }
+
+    // l
+    pub fn push_len(&mut self) {
+        self.entries.push_back(self.entries.len() as f64);
+    }
+}
+
+impl FromIterator<f64> for Stack {
+    fn from_iter<I: IntoIterator<Item = f64>>(iter: I) -> Self {
+        Self {
+            entries: iter.into_iter().collect(),
+            register: None,
+        }
+    }
+}
+
+impl IntoIterator for Stack {
+    type Item = f64;
+    type IntoIter = std::collections::vec_deque::IntoIter<f64>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.entries.into_iter()
+    }
+}
+
+impl Extend<f64> for Stack {
+    fn extend<I: IntoIterator<Item = f64>>(&mut self, iter: I) {
+        self.entries.extend(iter);
     }
 }
